156a666057064264b804dce60a34065dd6e1bbc7
sonybelone <sonymekeme@gmail.com>
2020-07-01 11:44:01 +0400

    etablissement de la connexion

--- client1.c
+++ client1.c
@@ -17,5 +17,19 @@ typedef struct User
 int main()
 {
 	int socketclient = socket(AF_INET, SOCK_STREAM, 0)//creation d'un socket duclient
+	struct sockaddr_in addrClient;
+	addrclient.sin_addr.s_addr = inet_addr("127.0.0.1");// adresse par defaut du serveur 
+	addrclient.sin_family = AF_INET;
+	addrclient.sin_port = htons(2500);//l'etablissement de la connection au port
+	connect(socketclient, (const struct sockaddr *)&addrclient, sizeof(addrclient));//l'etablissement de la connexion entre le client et l'adresse client au serveur
+	printf("vous etes maintenant connecte\n");
+	User user;
+	recv(socketclient, &user, sizeof(User), 0);//prendre le message au serveur et renvoyer au client
+	printf("%s \n",user.nom);
+	
+	close(socketclient);
+
+	return 0;
+	
 
 }
